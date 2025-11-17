5e65661f3ba974a2f37debf898b31a9673bff833
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2025-11-17 11:09:49 +0700

    menambahkan variable healthtext di objhealth

--- Assets/Game/Scripts/ObjHealth.cs
+++ Assets/Game/Scripts/ObjHealth.cs
@@ -1,16 +1,7 @@
 using UnityEngine;
+using TMPro;
 
 public class ObjHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TMP_Text HealthText;
 }
